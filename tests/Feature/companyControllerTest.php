2b690427da65ee64f24ba13da4471221ee6e82f0
Maxime CAVARROC <maxime.cavarroc@viacesi.fr>
2021-03-29 16:40:10 +0200

    debut test

---
+++ tests/Feature/companyControllerTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class companyControllerTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testIndex()
+    {
+        $response = $this->get('companies/index');
+        $response->assertStatus(200);
+    }
+}
